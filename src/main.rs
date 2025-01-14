7d6382c7d934a8c29c45eab962c6225ce800036c
indra sadik <g3n1k@yahoo.com>
2025-01-14 12:42:09 +0700

    main.rs v03

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ static mut ITEMS: Vec<Item> = Vec::new();
 
 // Health check endpoint
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok().body("API is running v:02")
+    HttpResponse::Ok().body("API is running v:03")
 }
 
 // Get all items
